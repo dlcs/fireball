2b1053e063bb11e2b53b62034737c92d9f42658c
Adam Christie <fractos@gmail.com>
2017-04-03 21:01:20 +0100

    fix.

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,16 +1,4 @@
 from fireball import app as application
-import logging
-
-def setup_logging():
-    """example docstring"""
-    logging.basicConfig(filename="fireball.log",
-                        filemode='a',
-                        level=logging.DEBUG,
-                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s', )
-    logging.getLogger('boto').setLevel(logging.ERROR)
-    logging.getLogger('botocore').setLevel(logging.ERROR)
-    logging.getLogger('werkzeug').setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
-    setup_logging()
     application.run(debug=True)
