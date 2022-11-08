9d762e111901fcec10f53b1f428224a38d89d7eb
Donald Gray <donald.gray@digirati.com>
2022-11-08 12:04:52 +0000

    Default envvars, update readme

--- app/settings.py
+++ app/settings.py
@@ -4,7 +4,7 @@ import distutils.util
 
 DEBUG = bool(distutils.util.strtobool(os.getenv("DEBUG", "True")))
 
-WORK_FOLDER = os.getenv('FIREBALL_WORK_FOLDER')
-DOWNLOAD_POOL_SIZE = int(os.getenv('FIREBALL_DOWNLOAD_POOL_SIZE'))
+WORK_FOLDER = os.getenv('FIREBALL_WORK_FOLDER', '/tmp')
+DOWNLOAD_POOL_SIZE = int(os.getenv('FIREBALL_DOWNLOAD_POOL_SIZE', '50'))
 DEFAULT_DPI = 300
 PDF_COMPLIANCE_VERSION = "%PDF-1.3"
